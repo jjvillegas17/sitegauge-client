b38224b3a0822056b8a630015578b2b3eff7e925
jjvillegas17 <jgvillegas@up.edu.ph>
2019-04-18 01:12:56 +0800

    add upload twitter csv functionality

--- src/components/UploadAnalytics.js
+++ src/components/UploadAnalytics.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import Menu from './Menu';
-import { Grid } from 'semantic-ui-react';
-
+import axios from 'axios';
+import queryString from 'query-string';
 /* make a home component then accept a props that will
    identify which is to be rendered in the content part
 */
 
 class UploadAnalytics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: 0,
+            file: '',
+            accounts: [],
+            pages: [],
+            gaccounts:[],
+            selectedAccount: '',
+        }
+    }
+
+    handleUpload = (e) => {
+        this.setState({file: e.target.files[0]});
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const id = this.refs.account.value;
+        let formData = new FormData();
+        formData.append('file', this.state.file);
+
+        console.log(formData);
+        if(this.state.type === 2){
+            axios.post(`https://sitegauge.io/api/twitter/${id}/upload`,
+                formData,
+                {
+                    headers:{
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+              )
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => { 
+                console.log(error); 
+            });
+        }
+    }
+
+    changeType = (e) => {
+        if(e.target.value === 0){
+
+        }
+        else if(e.target.value === 1){
+
+        }
+        else{
+            this.getTwitterAccts();
+        }
+        this.setState({type: parseInt(e.target.value,10)});
+    }
+
+    getTwitterAccts = () => {
+        const userId = localStorage.getItem("userId");
+        axios.get(`https://sitegauge.io/api/twitter/${userId}/accounts`)
+            .then((res) => {
+                console.log(res);
+                this.setState({ accounts: res.data});
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    getAccounts = () => {
+        if(this.state.accounts.length !== 0){
+            return (this.state.accounts.map(account => {
+                if(this.state.type === 2){
+                    return <option value={account.id} key={account.id}>@{account.username}</option>
+                }
+                else if(this.state.type === 1){
+                    return <option value={account.id} key={account.id}>{account.page_name}</option>
+                }
+                else{ // google
+
+                } 
+            }
+            ));
+        }
+        else{
+            return <option value="none" selected>Select Account</option>;
+        }
+    }
+
   render() {
     return (
         <div>
@@ -38,21 +124,48 @@ class UploadAnalytics extends Component {
                                 <form className="ui form">
                                     <div className="field">
                                         <label>Type</label>
-                                        <select class="ui fluid dropdown">
-                                            <option value="Google">Google</option>
-                                            <option value="Facebook">Facebook</option>
-                                            <option value="Twitter">Twitter</option>
+                                        <select className="ui fluid dropdown"
+                                            onChange={this.changeType}
+                                        >
+                                            <option value="0">Google</option>
+                                            <option value="1">Facebook</option>
+                                            <option value="2">Twitter</option>
                                         </select>
                                     </div>
+                                     <div className="field">
+                                        <div className="row">
+                                            <strong>Accounts</strong>
+                                            <select className="ui fluid dropdown" 
+                                                ref="account" 
+                                                onChange={this.handleAccountChange}
+                                            >
+                                                {this.getAccounts()}
+                                            </select>
+                                        </div>
+                                    </div>
                                     <div className="field">
-                                        <Grid>
-                                            <Grid.Column textAlign="center">
-                                                <label htmlFor="file" class="ui icon button">
-                                                    <i class="file icon"></i>
+                                        <div className="row">
+                                            <label htmlFor="file" className="ui icon button">
+                                                    <i className="file icon"></i>
                                                     Open File</label>
-                                                <input type="file" id="file" style={{display:"none"}}/>   
-                                            </Grid.Column>
-                                        </Grid> 
+                                            <input type="file" 
+                                                id="file" 
+                                                onChange={this.handleUpload}
+                                                style={{display:"none"}}
+                                            />
+                                        </div>
+                                        <div className="row">
+                                                {'  '}{this.state.file.name}
+                                        </div>
+                                        <div className="row">
+                                            <button
+                                                className="ui button"
+                                                onClick={this.handleSubmit}
+                                                style={{ width: "100px", marginTop: "15px"}}
+                                            >
+                                                Upload
+                                            </button>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
