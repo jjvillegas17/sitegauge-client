bddbef20332c724018871ab6179e2b8565d4001e
jjvillegas17 <jgvillegas@up.edu.ph>
2019-04-21 01:58:23 +0800

    add tweet metrics in the dashboard

---
+++ src/components/TweetMetric.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TweetMetric = (props) => {
+	console.log(props);
+	return (
+		<tr>
+	      <td><a href={props.props.link}>{props.props.text.substring(0,100)}</a></td>
+	      <td>{props.props.likes}</td>
+	      <td>{props.props.retweets}</td>
+	      <td>{props.props.impressions}</td>
+	      <td>{props.props.engagements}</td>
+	      <td>{props.props.engagement_rate}</td>
+	    </tr>
+	)
+}
+
+export default TweetMetric;
---
+++ src/components/TwitterAccount.js
@@ -0,0 +1,93 @@
+import React, { Component, Fragment } from 'react';
+import TweetMetric from './TweetMetric';
+import axios from 'axios';
+
+
+class TwitterAccount extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username:props.username,
+            id: props.id,
+            followers: props.followers,
+            following: props.following,
+            tweets: props.tweets,
+            tweetMetrics: [],
+        }
+    }
+
+    async componentDidMount(){
+        await this.fetchMetrics();
+    }
+
+    fetchMetrics = () => {
+        axios.get(`https://sitegauge.io/api/twitter/${this.state.id}/tweet-metrics`)
+            .then((res) => {
+                // console.log(res.data);
+                this.setState({tweetMetrics: res.data});
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    render(){
+    	return (
+    		<Fragment>
+    			<div className="row">
+                    <h2 className="ui header">@{this.state.username}</h2>
+                </div>
+                <div className="ui six column centered row">
+	                <div className="column">
+                		<div className="ui card" style={{ width: "150px"}}>
+                			<div className="content">
+							    <div class="header">{this.state.tweets}</div>
+							    <div class="meta">Tweets</div>
+							</div>
+                		</div>
+	              	</div>
+	              	<div className="column">
+	              		<div className="ui card" style={{ width: "150px"}}>
+                			<div className="content">
+							    <div className="header">{this.state.following}</div>
+							    <div className="meta">Following</div>
+							</div>
+                		</div>
+	              	</div>
+	              	<div className="column">
+	              		<div className="ui card" style={{ width: "150px"}}>
+                			<div className="content">
+							    <div className="header">{this.state.followers}</div>
+							    <div className="meta">Followers</div>
+							</div>
+                		</div>
+	              	</div>
+                </div>
+                <div className="ui row">
+                	<table className="ui striped table">
+                		<thead>
+						    <tr>
+						      <th>Tweet</th>
+                              <th>Likes</th>
+                              <th>Retweets</th>
+						      <th>Impressions</th>
+						      <th>Engagements</th>
+						      <th>Engagement Rate</th>
+						    </tr>
+					    </thead>
+					    <tbody>
+                        {
+                            this.state.tweetMetrics.map(tweetMetric => {
+                                return <TweetMetric props={tweetMetric} key={tweetMetric.id}/>
+                            })
+                        }
+					    </tbody>
+                	</table>
+                </div>
+    		</Fragment>
+    	)
+    }
+}
+
+export default TwitterAccount;
